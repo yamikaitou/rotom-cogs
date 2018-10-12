110f7454f9c8b256c67bd3589f97401934c47ecc
Ryan <yamikaitou@gmail.com>
2018-10-12 09:56:26 -0500

    Add config settings

--- info/info.py
+++ info/info.py
@@ -1,14 +1,34 @@
 import discord
-from redbot.core import commands, __version__
+from redbot.core import commands, __version__, checks, Config
 import sys
+from typing import Union
 
 
 class Info(getattr(commands, "Cog", object)):
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=602700309)
+        default_guild = {
+            "channel": {
+                "role": None,
+                "ex": None,
+                "raid": None,
+                "social": None,
+                "research": None,
+            },
+            "setting": {
+                "egg": None,
+                "boss": None,
+                "research": None,
+            }
+        }
+        self.config.register_guild(**default_guild)
 
     @commands.command(name="info", aliases=["about"])
     async def _info(self, ctx):
+        """
+        Displays Info about Rotom
+        """
         red_pypi = "https://pypi.python.org/pypi/Red-DiscordBot"
         dpy_repo = "https://github.com/Rapptz/discord.py"
         python_url = "https://www.python.org/"
@@ -57,3 +77,115 @@ class Info(getattr(commands, "Cog", object)):
         embed.add_field(name="discord.py", value=dpy_version, inline=True)
         embed.add_field(name="Red", value=red_version, inline=True)
         await ctx.send(embed=embed)
+
+    @commands.group()
+    @commands.guild_only()
+    @checks.admin_or_permissions(manage_server=True)
+    async def pogo(self, ctx):
+        """
+        Set various settings
+        """
+        if ctx.invoked_subcommand is None:
+            guild = ctx.guild
+            crole = await self.config.guild(guild).channel.role()
+            cex = await self.config.guild(guild).channel.ex()
+            craid = await self.config.guild(guild).channel.raid()
+            csocial = await self.config.guild(guild).channel.social()
+            cresearch = await self.config.guild(guild).channel.research()
+            segg = await self.config.guild(guild).setting.egg()
+            sboss = await self.config.guild(guild).setting.boss()
+
+            await ctx.send(
+                "```"
+                "Channel Locations\n"
+                "Role Assign: {}\n"
+                "EX Raids: {}\n"
+                "Raids: {}\n"
+                "Social Media: {}\n"
+                "Research: {}\n"
+                "\n"
+                "Various Settings\n"
+                "Raid Egg Timer: {}\n"
+                "Raid Boss Timer: {}\n"
+                "```".format(
+                    guild.get_channel(crole), 
+                    guild.get_channel(cex), 
+                    guild.get_channel(craid), 
+                    guild.get_channel(csocial), 
+                    guild.get_channel(cresearch),
+                    segg, sboss
+                    )
+            )
+
+        pass
+
+    @pogo.command()
+    @commands.guild_only()
+    async def role(self, ctx, channel: discord.TextChannel = None):
+        """
+        Set the channel for Role Assignment posts
+        """
+        if channel is None:
+            await self.config.guild(ctx.guild).channel.role.set(ctx.channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel.role.set(channel.id)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def ex(self, ctx, channel: discord.TextChannel = None):
+        """
+        Set the channel for EX-Raid Pass posts
+        """
+        if channel is None:
+            await self.config.guild(ctx.guild).channel.ex.set(ctx.channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel.ex.set(channel.id)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def raids(self, ctx, channel: discord.TextChannel = None):
+        """
+        Set the channel for Raid Channel Creation posts
+        """
+        if channel is None:
+            await self.config.guild(ctx.guild).channel.raid.set(ctx.channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel.raid.set(channel.id)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def social(self, ctx, channel: discord.TextChannel = None):
+        """
+        Set the channel for Social Media Postings
+        """
+        if channel is None:
+            await self.config.guild(ctx.guild).channel.social.set(ctx.channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel.social.set(channel.id)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def research(self, ctx, channel: discord.TextChannel = None):
+        """
+        Set the channel for Field Research posts
+        """
+        if channel is None:
+            await self.config.guild(ctx.guild).channel.research.set(ctx.channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel.research.set(channel.id)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def egg(self, ctx, time):
+        """
+        Set the time for Raid Eggs (in minutes)
+        """
+        await self.config.guild(ctx.guild).setting.egg.set(time)
+
+    @pogo.command()
+    @commands.guild_only()
+    async def boss(self, ctx, time):
+        """
+        Set the time for Raid Bosses (in minutes)
+        """
+        await self.config.guild(ctx.guild).setting.boss.set(time)
